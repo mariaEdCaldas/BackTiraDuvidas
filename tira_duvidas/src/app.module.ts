825629987dc7710f7a5209067641d50e26b9f320
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2023-11-09 08:11:40 -0400

    user auth

--- tira_duvidas/src/app.module.ts
+++ tira_duvidas/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import databaseConfig from './config/database.config';
+import authConfig from './config/auth.config';
+import appConfig from './config/app.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { MailModule } from './mail/mail.module';
+// import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, authConfig, appConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    // ScheduleModule.forRoot(),
+    UserModule,
+    AuthModule,
+    MailModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
